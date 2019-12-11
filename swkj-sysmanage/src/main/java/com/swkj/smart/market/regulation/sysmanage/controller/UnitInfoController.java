c6203669eeef03ba1d104072d295bcc0ed47d581
swp12345 <swp@cnswai.com>
2019-12-11 11:49:22 +0800

    单位信息查询

--- swkj-sysmanage/src/main/java/com/swkj/smart/market/regulation/sysmanage/controller/UnitInfoController.java
+++ swkj-sysmanage/src/main/java/com/swkj/smart/market/regulation/sysmanage/controller/UnitInfoController.java
@@ -1,6 +1,14 @@
 package com.swkj.smart.market.regulation.sysmanage.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.swkj.smart.market.regulation.dto.HttpResult;
+import com.swkj.smart.market.regulation.model.UnitInfo;
+import com.swkj.smart.market.regulation.sysmanage.service.IUnitInfoService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +24,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/unit-info")
 public class UnitInfoController {
+
+    @Autowired
+    private IUnitInfoService iUnitInfoService;
+
+    @ApiOperation(value = "所有单位查询")
+    @GetMapping(value = "/selectUnitInfos")
+    public HttpResult selectUnitInfos(int currentPage){
+        IPage<UnitInfo> infoIPage = iUnitInfoService.selectUnitInfos(currentPage);
+        return HttpResult.ok(infoIPage);
+    }
 
 }
 
